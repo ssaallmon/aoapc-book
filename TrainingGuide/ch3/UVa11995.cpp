dfb07f61e4ae404e6c272421d6ab9471186e25f8
yzs <921546507@qq.com>
2022-05-14 17:49:31 +0800

    I Can Guess the Data Structure!

--- TrainingGuide/ch3/UVa11995.cpp
+++ TrainingGuide/ch3/UVa11995.cpp
@@ -11,21 +11,21 @@ int cmd, x, idx, ok[3];
 
 int main() {
   int n;
-  while (~scanf("%d", &n)) {
+  while(~scanf("%d", &n)) {
     stack<int> s;
     queue<int> q;
     priority_queue<int> pq;
     ok[0] = ok[1] = ok[2] = 1;
-    for (int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; ++i) {
       scanf("%d%d", &cmd, &x);
-      if (cmd == 1) {
+      if(cmd == 1) {
         s.push(x);
         q.push(x);
         pq.push(x);
       } else {
-        if (s.empty() || s.top() != x) ok[0] = 0;
-        if (q.empty() || q.front() != x) ok[1] = 0;
-        if (pq.empty() || pq.top() != x) ok[2] = 0;
+        if(s.empty() || s.top() != x) ok[0] = 0;
+        if(q.empty() || q.front() != x) ok[1] = 0;
+        if(pq.empty() || pq.top() != x) ok[2] = 0;
         s.pop();
         q.pop();
         pq.pop();
