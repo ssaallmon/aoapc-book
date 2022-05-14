f8af3c3999e80ae700eb356c23d98255b12e8473
yzs <921546507@qq.com>
2022-05-14 17:46:57 +0800

    4 Values whose Sum is 0

--- aoapc-bac2nd/ch8/UVa1152.cpp
+++ aoapc-bac2nd/ch8/UVa1152.cpp
@@ -1,5 +1,5 @@
-#include <algorithm>
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
 const int maxn = 4005;
@@ -7,11 +7,11 @@ int n, A[maxn], B[maxn], C[maxn], D[maxn];
 
 int sum() {
   int s[n * n], cnt = 0, ans = 0;
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j) s[cnt++] = A[i] + B[j];
+  for(int i = 0; i < n; ++i)
+    for(int j = 0; j < n; ++j) s[cnt++] = A[i] + B[j];
   sort(s, s + cnt);
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j)
+  for(int i = 0; i < n; ++i)
+    for(int j = 0; j < n; ++j)
       ans += upper_bound(s, s + cnt, -C[i] - D[j]) - lower_bound(s, s + cnt, -C[i] - D[j]);
   return ans;
 }
@@ -19,9 +19,9 @@ int sum() {
 int main() {
   int T;
   scanf("%d", &T);
-  while (T--) {
+  while(T--) {
     scanf("%d", &n);
-    for (int i = 0; i < n; ++i) scanf("%d%d%d%d", &A[i], &B[i], &C[i], &D[i]);
+    for(int i = 0; i < n; ++i) scanf("%d%d%d%d", &A[i], &B[i], &C[i], &D[i]);
     printf("%d%s", sum(), T ? "\n\n" : "\n");
   }
   return 0;
