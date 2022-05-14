188d1ac16d3249e42ff9e5cf481ea96f5ac62277
yzs <921546507@qq.com>
2022-05-14 17:02:11 +0800

    The Blocks Problem

--- aoapc-bac2nd/ch5/UVa101.cpp
+++ aoapc-bac2nd/ch5/UVa101.cpp
@@ -9,7 +9,7 @@ stack<int> pile[maxn];
 
 void clear_above(int p) {
   int pa = pos[p];
-  while (!pile[pa].empty() && pile[pa].top() != p) {
+  while(!pile[pa].empty() && pile[pa].top() != p) {
     int pb = pile[pa].top();
     pile[pb].push(pb);
     pos[pb] = pb;
@@ -20,28 +20,28 @@ void clear_above(int p) {
 void pile_onto(int a, int b) {
   stack<int> t;
   int pa = pos[a], pb = pos[b];
-  while (!pile[pa].empty()) {
+  while(!pile[pa].empty()) {
     int p = pile[pa].top();
     t.push(p);
     pos[p] = pb;
     pile[pa].pop();
-    if (p == a) break;
+    if(p == a) break;
   }
-  while (!t.empty()) {
+  while(!t.empty()) {
     pile[pb].push(t.top());
     t.pop();
   }
 }
 
 void print() {
-  for (int i = 0; i < n; ++i) {
+  for(int i = 0; i < n; ++i) {
     stack<int> t;
-    while (!pile[i].empty()) {
+    while(!pile[i].empty()) {
       t.push(pile[i].top());
       pile[i].pop();
     }
     printf("%d:", i);
-    while (!t.empty()) {
+    while(!t.empty()) {
       printf(" %d", t.top());
       t.pop();
     }
@@ -53,15 +53,15 @@ int main() {
   int a, b;
   char s1[5], s2[5];
   scanf("%d", &n);
-  for (int i = 0; i < n; ++i) {
+  for(int i = 0; i < n; ++i) {
     pos[i] = i;
     pile[i].push(i);
   }
-  while (scanf("%s", s1) && s1[0] != 'q') {
+  while(scanf("%s", s1) && s1[0] != 'q') {
     scanf("%d%s%d", &a, s2, &b);
-    if (pos[a] == pos[b]) continue;
-    if (s1[0] == 'm') clear_above(a);
-    if (s2[1] == 'n') clear_above(b);
+    if(pos[a] == pos[b]) continue;
+    if(s1[0] == 'm') clear_above(a);
+    if(s2[1] == 'n') clear_above(b);
     pile_onto(a, b);
   }
   print();
