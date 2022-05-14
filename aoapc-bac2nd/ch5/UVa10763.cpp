732d9e69e3c5634c2fcba700c4dafeb46bf38a11
yzs <921546507@qq.com>
2022-05-14 17:13:04 +0800

    Foreign Exchange

--- aoapc-bac2nd/ch5/UVa10763.cpp
+++ aoapc-bac2nd/ch5/UVa10763.cpp
@@ -6,19 +6,16 @@ const int maxn = 5e5 + 5;
 int n, A, B, num[maxn];
 
 int main() {
-  while (scanf("%d", &n) && n) {
+  while(scanf("%d", &n) && n) {
     int ok = 1;
     memset(num, 0, sizeof(num));
-    for (int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; ++i) {
       scanf("%d%d", &A, &B);
       --num[A];
       ++num[B];
     }
-    for (int i = 0; i < maxn; ++i) {
-      if (num[i]) {
-        ok = 0;
-        break;
-      }
+    for(int i = 0; i < maxn; ++i) {
+      if(num[i]) { ok = 0; break; }
     }
     puts(ok ? "YES" : "NO");
   }
