7077fc039f9cc12dd254c55797df54cbb793f8c1
yzs <921546507@qq.com>
2022-05-14 16:36:50 +0800

    Kickdown

--- aoapc-bac2nd/ch3/UVa1588.cpp
+++ aoapc-bac2nd/ch3/UVa1588.cpp
@@ -8,11 +8,11 @@ int min(const int &i, const int &j) { return i < j ? i : j; }
 
 int kickdown(char *s1, char *s2) {
   int n1 = strlen(s1), n2 = strlen(s2), len = 0;
-  for (int i = 0; i <= n1; ++i) {
+  for(int i = 0; i <= n1; ++i) {
     int ok = 1;
-    for (int j = 0; ok && i + j < n1 && j < n2; ++j)
-      if (s1[i + j] - '0' + s2[j] - '0' > 3) ok = 0;
-    if (ok) {
+    for(int j = 0; ok && i + j < n1 && j < n2; ++j)
+      if(s1[i + j] - '0' + s2[j] - '0' > 3) ok = 0;
+    if(ok) {
       len = n1 - i > n2 ? n1 : i + n2;
       break;
     }
@@ -21,7 +21,7 @@ int kickdown(char *s1, char *s2) {
 }
 
 int main() {
-  while (scanf("%s%s", bottom, top) == 2)
+  while(scanf("%s%s", bottom, top) == 2)
     printf("%d\n", min(kickdown(bottom, top), kickdown(top, bottom)));
   return 0;
 }
