ef1ffc22bd129dc2cf794712297a3ecf874faa56
yzs <921546507@qq.com>
2022-05-14 16:27:50 +0800

    Digit Counting

--- aoapc-bac2nd/ch3/UVa1225.cpp
+++ aoapc-bac2nd/ch3/UVa1225.cpp
@@ -4,18 +4,18 @@
 int main() {
   int T;
   scanf("%d", &T);
-  while (T--) {
+  while(T--) {
     int N, n[10];
     scanf("%d", &N);
     memset(n, 0, sizeof(n));
-    for (int i = 1; i <= N; ++i) {
+    for(int i = 1; i <= N; ++i) {
       int v = i;
-      while (v) {
+      while(v) {
         ++n[v % 10];
         v /= 10;
       }
     }
-    for (int i = 0; i < 10; ++i) printf(i ? " %d" : "%d", n[i]);
+    for(int i = 0; i < 10; ++i) printf(i ? " %d" : "%d", n[i]);
     putchar('\n');
   }
   return 0;
