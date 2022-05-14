b502be0a260d703ed95b80fac334485e2ec608f2
yzs <921546507@qq.com>
2022-05-14 17:42:53 +0800

    Play on Words

--- aoapc-bac2nd/ch6/UVa10129.cpp
+++ aoapc-bac2nd/ch6/UVa10129.cpp
@@ -8,9 +8,9 @@ int find(int i) { return p[i] == i ? i : p[i] = find(p[i]); }
 
 int euler() {
   int n1 = 0, n2 = 0;
-  for (int i = 0; i < 26; ++i) {
-    if (vis[i] && p[i] == i) ++n1;
-    if (!d[i] || d[i] == -1 || d[i] == 1) ++n2;
+  for(int i = 0; i < 26; ++i) {
+    if(vis[i] && p[i] == i) ++n1;
+    if(!d[i] || d[i] == -1 || d[i] == 1) ++n2;
   }
   return n1 == 1 && n2 == 26;
 }
@@ -18,13 +18,13 @@ int euler() {
 int main() {
   int T;
   scanf("%d", &T);
-  while (T--) {
+  while(T--) {
     int N;
     scanf("%d", &N);
     memset(d, 0, sizeof(d));
     memset(vis, 0, sizeof(vis));
-    for (int i = 0; i < 26; ++i) p[i] = i;
-    for (int i = 0; i < N; ++i) {
+    for(int i = 0; i < 26; ++i) p[i] = i;
+    for(int i = 0; i < N; ++i) {
       scanf("%s", s);
       int u = s[0] - 'a', v = s[strlen(s) - 1] - 'a';
       vis[u] = vis[v] = 1;
