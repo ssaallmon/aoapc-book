83d261693d58815fa335e792726b61cebf33e5b8
yzs <921546507@qq.com>
2022-05-14 17:45:17 +0800

    Stacks of Flapjacks

--- aoapc-bac2nd/ch8/UVa120.cpp
+++ aoapc-bac2nd/ch8/UVa120.cpp
@@ -12,26 +12,22 @@ void swap(int &a, int &b) {
 
 void flip(int p) {
   printf("%d ", n - p);
-  for (int i = 0; i < p - i; ++i) swap(a[i], a[p - i]);
+  for(int i = 0; i < p - i; ++i) swap(a[i], a[p - i]);
 }
 
 int main() {
   string s;
-  while (getline(cin, s)) {
+  while(getline(cin, s)) {
     n = 0;
     cout << s << '\n';
     stringstream ss(s);
-    while (ss >> a[n]) ++n;
-    for (int i = n - 1; i; --i) {
+    while(ss >> a[n]) ++n;
+    for(int i = n - 1; i; --i) {
       int max = a[i], p = i;
-      for (int j = i - 1; j >= 0; --j) {
-        if (a[j] > max) {
-          max = a[j];
-          p = j;
-        }
-      }
-      if (p == i) continue;
-      if (p) flip(p);
+      for(int j = i - 1; j >= 0; --j)
+        if(a[j] > max) { max = a[j]; p = j; }
+      if(p == i) continue;
+      if(p) flip(p);
       flip(i);
     }
     puts("0");
