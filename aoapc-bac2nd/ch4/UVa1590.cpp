9cbd6ebe94c4da29955a94dd2b8a6c7f324d08af
yzs <921546507@qq.com>
2022-05-14 16:59:12 +0800

    IP Networks

--- aoapc-bac2nd/ch4/UVa1590.cpp
+++ aoapc-bac2nd/ch4/UVa1590.cpp
@@ -7,8 +7,8 @@ int ip[4][1005], addr[4], mask[4];
 
 int lcp(int a, int b) {  // longest common prefix
   int n = 0;
-  for (int i = 0; i < 8; ++i) {
-    if ((a & 128) ^ (b & 128)) break;
+  for(int i = 0; i < 8; ++i) {
+    if((a & 128) ^ (b & 128)) break;
     a <<= 1;
     b <<= 1;
     ++n;
@@ -18,16 +18,16 @@ int lcp(int a, int b) {  // longest common prefix
 
 int main() {
   int m;
-  while (~scanf("%d", &m)) {
+  while(~scanf("%d", &m)) {
     memset(mask, 0, sizeof(mask));
     memset(addr, 0, sizeof(addr));
-    for (int i = 0; i < m; ++i)
+    for(int i = 0; i < m; ++i)
       scanf("%d.%d.%d.%d", &ip[0][i], &ip[1][i], &ip[2][i], &ip[3][i]);
-    for (int i = 0; i < 4; ++i) {
+    for(int i = 0; i < 4; ++i) {
       std::sort(ip[i], ip[i] + m);
       mask[i] = mask_table[lcp(ip[i][0], ip[i][m - 1])];
       addr[i] = mask[i] & ip[i][0];
-      if (mask[i] ^ 255) break;
+      if(mask[i] ^ 255) break;
     }
     printf("%d.%d.%d.%d\n", addr[0], addr[1], addr[2], addr[3]);
     printf("%d.%d.%d.%d\n", mask[0], mask[1], mask[2], mask[3]);
