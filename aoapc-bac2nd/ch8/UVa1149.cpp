f51eb0eaebc6a06aeaf418875339a148df12c27e
yzs <921546507@qq.com>
2022-05-14 17:46:09 +0800

    Bin Packing

--- aoapc-bac2nd/ch8/UVa1149.cpp
+++ aoapc-bac2nd/ch8/UVa1149.cpp
@@ -7,8 +7,8 @@ int n, l, a[maxn];
 int pack() {
   int q = 0, i = 0, j = n - 1;
   std::sort(a, a + n);
-  while (i <= j) {
-    if (a[i] + a[j--] <= l) ++i;
+  while(i <= j) {
+    if(a[i] + a[j--] <= l) ++i;
     ++q;
   }
   return q;
@@ -17,9 +17,9 @@ int pack() {
 int main() {
   int T;
   scanf("%d", &T);
-  while (T--) {
+  while(T--) {
     scanf("%d%d", &n, &l);
-    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+    for(int i = 0; i < n; ++i) scanf("%d", &a[i]);
     printf("%d%s", pack(), T ? "\n\n" : "\n");
   }
   return 0;
