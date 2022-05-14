c0f3b9c041a1a34f279a2081787e574748b2face
yzs <921546507@qq.com>
2022-05-14 16:37:38 +0800

    WERTYU

--- aoapc-bac2nd/ch3/UVa10082.cpp
+++ aoapc-bac2nd/ch3/UVa10082.cpp
@@ -5,7 +5,7 @@ const char *s = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
 
 int main() {
   char c;
-  while ((c = getchar()) != EOF)
+  while((c = getchar()) != EOF)
     putchar(c == ' ' || c == '\n' ? c : s[strchr(s, c) - s - 1]);
   return 0;
 }
