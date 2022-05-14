600bf71e9cccf57198b3e00a91f04799f1a3b406
yzs <921546507@qq.com>
2022-05-14 16:34:21 +0800

    Molar mass

--- aoapc-bac2nd/ch3/UVa1586.cpp
+++ aoapc-bac2nd/ch3/UVa1586.cpp
@@ -8,18 +8,17 @@ const double w[] = {12.01, 1.008, 16.00, 14.01};
 int main() {
   int T;
   scanf("%d", &T);
-  while (T--) {
+  while(T--) {
     char s[85];
     scanf("%s", s);
     double sum = 0.;
     int i = 0, n = strlen(s);
-    for (int i = 0; i < n; ++i) {
-      if (isdigit(s[i])) continue;
+    for(int i = 0; i < n; ++i) {
+      if(isdigit(s[i])) continue;
       int j = strchr(a, s[i]) - a;
-      if (!isdigit(s[i + 1]))
-        sum += w[j];
+      if(!isdigit(s[i + 1])) sum += w[j];
       else {
-        if (isdigit(s[i + 1]) && isdigit(s[i + 2]))
+        if(isdigit(s[i + 1]) && isdigit(s[i + 2]))
           sum += w[j] * ((s[i + 1] - '0') * 10 + (s[i + 2] - '0'));
         else
           sum += w[j] * (s[i + 1] - '0');
