d8d31b7bbd5197924e0de1fc2bc28f371a9e380d
yzs <921546507@qq.com>
2022-05-14 17:36:15 +0800

    Parentheses Balance

--- aoapc-bac2nd/ch6/UVa673.cpp
+++ aoapc-bac2nd/ch6/UVa673.cpp
@@ -6,13 +6,10 @@ char str[maxl], s[maxl];
 
 int check() {
   int top = -1, m = strlen(str);
-  for (int i = 0; i < m - 1; ++i) {
-    if (str[i] == '(' || str[i] == '[')
-      s[++top] = str[i];
-    else if (str[i] == ')' && s[top] == '(' || str[i] == ']' && s[top] == '[')
-      --top;
-    else
-      return 0;
+  for(int i = 0; i < m - 1; ++i) {
+    if(str[i] == '(' || str[i] == '[') s[++top] = str[i];
+    else if (str[i] == ')' && s[top] == '(' || str[i] == ']' && s[top] == '[') --top;
+    else return 0;
   }
   return top < 0;
 }
@@ -21,7 +18,7 @@ int main() {
   int n;
   scanf("%d", &n);
   getchar();
-  while (n--) {
+  while(n--) {
     fgets(str, maxl, stdin);
     memset(s, 0, sizeof(s));
     puts(check() ? "Yes" : "No");
