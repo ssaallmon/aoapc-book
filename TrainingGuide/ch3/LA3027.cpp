9dde1d2dcf80e6420fc794bf1bd8c993fbda8d3c
yzs <921546507@qq.com>
2022-05-27 20:47:56 +0800

    Corporative Network

--- TrainingGuide/ch3/LA3027.cpp
+++ TrainingGuide/ch3/LA3027.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
+#include <cmath>
 
 const int MOD = 1e3;
 const int maxn = 2e4 + 5;
 int N, p[maxn], d[maxn];
-
-int abs(int i) { return (i ^ (i >> 31)) - (i >> 31); }
 
 int find(int u) {
   if(p[u] == u) return u;
@@ -24,11 +23,7 @@ int main() {
     while(scanf("%s", s) && s[0] != 'O') {
       scanf("%d", &u);
       if(s[0] == 'E') { find(u); printf("%d\n", d[u]); }
-      else {
-        scanf("%d", &v);
-        p[u] = v;
-        d[u] = abs(u - v) % MOD;
-      }
+      else { scanf("%d", &v); p[u] = v; d[u] = abs(u - v) % MOD; }
     }
   }
   return 0;
