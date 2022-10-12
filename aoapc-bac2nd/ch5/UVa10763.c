0a7cbd1db9de5398147ae6143d1ec0c5b7c5b43f
yzs <921546507@qq.com>
2022-10-12 13:46:41 +0800

    Foreign Exchange

--- aoapc-bac2nd/ch5/UVa10763.c
+++ aoapc-bac2nd/ch5/UVa10763.c
@@ -1,22 +1,20 @@
-#include <cstdio>
-#include <cstring>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#define MAXN 500005
 
-const int maxn = 5e5 + 5;
-int n, A, B, num[maxn];
+int A, B, num[MAXN];
 
 int main() {
+  int n;
   while(scanf("%d", &n) && n) {
     int ok = 1;
     memset(num, 0, sizeof(num));
     for(int i = 0; i < n; ++i) {
       scanf("%d%d", &A, &B);
-      --num[A];
-      ++num[B];
+      --num[A]; ++num[B];
     }
-    for(int i = 0; i < maxn; ++i) {
+    for(int i = 0; i < MAXN; ++i)
       if(num[i]) { ok = 0; break; }
-    }
     puts(ok ? "YES" : "NO");
   }
   return 0;
