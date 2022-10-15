d02cc93ddae3681d9c07a1b1dda2c0b958fbfe8e
yzs <921546507@qq.com>
2022-10-15 16:56:57 +0800

    All in All

--- aoapc-bac2nd/ch3/UVa10340.c
+++ aoapc-bac2nd/ch3/UVa10340.c
@@ -1,11 +1,10 @@
-#include <cstdio>
-#include <cstring>
-
-const int maxn = 1e5 + 5;
+#include <stdio.h>
+#include <string.h>
+#define MAXN 100005
 
 int main() {
-  char s[maxn], t[maxn];
-  while(scanf("%s%s", s, t) == 2) {
+  char s[MAXN], t[MAXN];
+  while(~scanf("%s%s", s, t)) {
     int i = 0, n = strlen(t);
     for(int j = 0; j < n; ++j)
       if(t[j] == s[i]) ++i;
