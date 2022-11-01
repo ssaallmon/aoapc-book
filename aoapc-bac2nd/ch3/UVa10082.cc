4c38eb2ad951cfd970f742e0e41ed211d3533de6
yzs <921546507@qq.com>
2022-11-01 18:35:04 +0800

    WERTYU

--- aoapc-bac2nd/ch3/UVa10082.cc
+++ aoapc-bac2nd/ch3/UVa10082.cc
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
 const char *s = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
 
 int main() {
   char c;
-  while((c = getchar()) != EOF)
+  while(~(c = getchar()))
     putchar(c == ' ' || c == '\n' ? c : s[strchr(s, c) - s - 1]);
   return 0;
 }
