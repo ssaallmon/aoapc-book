d60485a4ebc6a49903a4ef54510fc631bf7ebbce
yzs <921546507@qq.com>
2022-11-03 22:39:21 +0800

    Morse Mismatches

--- aoapc-bac2nd/ch4/UVa508.cc
+++ aoapc-bac2nd/ch4/UVa508.cc
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
 
 int n;
 char morse_code[128][10], context[105][15], morse_word[105][65], ans[15];
@@ -20,7 +20,10 @@ void match(char *s) {
       int l2 = strlen(morse_word[i]);
       if(!strncmp(morse_word[i], s, l1) || !strncmp(s, morse_word[i], l2)) {
         int diff = abs(l1 - l2);
-        if(min_diff >= diff) { min_diff = diff; strcpy(ans, context[i]); }
+        if(min_diff >= diff) {
+          min_diff = diff;
+          strcpy(ans, context[i]);
+        }
       }
     }
     strcat(ans, "?");
@@ -29,13 +32,18 @@ void match(char *s) {
 
 int main() {
   char c, s1[15], s2[65];
-  while(scanf("%s", &c) && c != '*') scanf("%s", morse_code[c]);
+  while(scanf("%s", &c) && c != '*')
+    scanf("%s", morse_code[c]);
   for(n = 0; scanf("%s", s1) && strcmp(s1, "*"); ++n) {
     strcpy(context[n], s1);
     memset(s2, 0, sizeof(s2));
-    for(int i = 0; i < strlen(s1); ++i) strcat(s2, morse_code[s1[i]]);
+    for(int i = 0; i < strlen(s1); ++i)
+      strcat(s2, morse_code[s1[i]]);
     strcpy(morse_word[n], s2);
   }
-  while(scanf("%s", s2) && strcmp(s2, "*")) { match(s2); printf("%s\n", ans); }
+  while(scanf("%s", s2) && strcmp(s2, "*")) {
+    match(s2);
+    printf("%s\n", ans);
+  }
   return 0;
 }
