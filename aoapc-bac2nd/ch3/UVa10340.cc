a7f11a1cf7462e5e9d50704f19de7ba3ab9d9d87
yzs <921546507@qq.com>
2022-11-04 21:52:22 +0800

    All in All

--- aoapc-bac2nd/ch3/UVa10340.cc
+++ aoapc-bac2nd/ch3/UVa10340.cc
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <cstring>
+
 const int maxn = 1e5+5;
+char s[maxn], t[maxn];
 
 int main() {
-  char s[maxn], t[maxn];
   while(~scanf("%s%s", s, t)) {
     int i = 0, n = strlen(t);
     for(int j = 0; j < n; ++j)
