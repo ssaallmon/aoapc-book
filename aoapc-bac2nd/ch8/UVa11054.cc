b83447e7a584b1902d0b7821953ee564813ae5eb
yzs <921546507@qq.com>
2022-11-04 11:31:25 +0800

    Wine trading in Gergovia

--- aoapc-bac2nd/ch8/UVa11054.cc
+++ aoapc-bac2nd/ch8/UVa11054.cc
@@ -1,12 +1,11 @@
 #include <cstdio>
 #include <cstdlib>
 
-int a, b, n;
-long long ans;
-
 int main() {
+  int n;
   while(scanf("%d", &n) && n) {
-    b = ans = 0;
+    int a, b = 0;
+    long long ans = 0;
     for(int i = 0; i < n; ++i) {
       scanf("%d", &a);
       b += a;
