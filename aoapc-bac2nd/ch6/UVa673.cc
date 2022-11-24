5456602dc7b812c44c4b152feb177b0c305fc228
yzs <921546507@qq.com>
2022-11-24 17:34:46 +0800

    Parentheses Balance

--- aoapc-bac2nd/ch6/UVa673.cc
+++ aoapc-bac2nd/ch6/UVa673.cc
@@ -2,13 +2,13 @@
 #include <cstring>
 
 const int maxl = 130;
-char str[maxl], s[maxl];
+char A[maxl], B[maxl];
 
 int check() {
-  int top = -1, m = strlen(str);
+  int top = -1, m = strlen(A);
   for(int i = 0; i < m - 1; ++i) {
-    if(str[i] == '(' || str[i] == '[') s[++top] = str[i];
-    else if (str[i] == ')' && s[top] == '(' || str[i] == ']' && s[top] == '[') --top;
+    if(A[i] == '(' || A[i] == '[') B[++top] = A[i];
+    else if (A[i] == ')' && B[top] == '(' || A[i] == ']' && B[top] == '[') --top;
     else return 0;
   }
   return top < 0;
@@ -19,8 +19,8 @@ int main() {
   scanf("%d", &n);
   getchar();
   while(n--) {
-    fgets(str, maxl, stdin);
-    memset(s, 0, sizeof(s));
+    fgets(A, maxl, stdin);
+    memset(B, 0, sizeof(B));
     puts(check() ? "Yes" : "No");
   }
   return 0;
