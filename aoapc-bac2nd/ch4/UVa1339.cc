c56526b21076846000e76dde8257e192418fc164
yzs <921546507@qq.com>
2022-12-07 14:05:37 +0800

    Ancient Cipher

--- aoapc-bac2nd/ch4/UVa1339.cc
+++ aoapc-bac2nd/ch4/UVa1339.cc
@@ -3,9 +3,10 @@
 #include <algorithm>
 const int maxn = 105;
 
+int n1[26], n2[26];
+char s1[maxn], s2[maxn];
+
 int main() {
-  int n1[26], n2[26];
-  char s1[maxn], s2[maxn];
   while(~scanf("%s%s", s1, s2)) {
     memset(n1, 0, sizeof(n1));
     memset(n2, 0, sizeof(n2));
@@ -18,7 +19,7 @@ int main() {
     std::sort(n2, n2 + 26);
     for(int i = 0; i < 26; ++i)
       if(n1[i] ^ n2[i]) { ok = 0; break; }
-    printf("%s\n", ok ? "YES" : "NO");
+    puts(ok ? "YES" : "NO");
   }
   return 0;
 }
