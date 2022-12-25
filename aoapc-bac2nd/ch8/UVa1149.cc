9ee8d5940bd485220229ef7f063435e0f0e801b0
yzs <921546507@qq.com>
2022-12-25 17:27:51 +0800

    Bin Packing

--- aoapc-bac2nd/ch8/UVa1149.cc
+++ aoapc-bac2nd/ch8/UVa1149.cc
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <algorithm>
 
-const int maxn = 1e5+5;
+const int maxn = 1e5 + 5;
 int a[maxn];
 
 int main() {
@@ -10,7 +10,7 @@ int main() {
   while(T--) {
     int n, l, i, j, q = 0;
     scanf("%d%d", &n, &l);
-    for(int i = 0; i < n; ++i) scanf("%d", &a[i]);
+    for(i = 0; i < n; ++i) scanf("%d", &a[i]);
     std::sort(a, a + n);
     i = 0, j = n - 1;
     while(i <= j) {
