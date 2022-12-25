70be344ff3b9e96016229414323c7976e8ea36e2
yzs <921546507@qq.com>
2022-12-25 16:01:49 +0800

    Circular Sequence

--- aoapc-bac2nd/ch3/UVa1584.cc
+++ aoapc-bac2nd/ch3/UVa1584.cc
@@ -1,12 +1,11 @@
 #include <cstdio>
 #include <cstring>
 
-const int maxl = 105;
-char s[maxl], n;
+char s[105], n;
 
 int less(int p, int q) {
   for(int i = 0; i < n; ++i) {
-    int v = s[(i+p)%n] - s[(i+q)%n];
+    int v = s[(i + p) % n] - s[(i + q) % n];
     if(v) return v < 0 ? p : q;
   }
   return p;
@@ -20,7 +19,7 @@ int main() {
     scanf("%s", s);
     n = strlen(s);
     for(int i = 1; i < n; ++i) ans = less(ans, i);
-    for(int i = 0; i < n; ++i) putchar(s[(i+ans)%n]);
+    for(int i = 0; i < n; ++i) putchar(s[(i + ans) % n]);
     putchar('\n');
   }
   return 0;
