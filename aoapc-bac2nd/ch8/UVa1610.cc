77de8f14fcf3d8553290c37f623385badd798c47
yzs <921546507@qq.com>
2022-12-27 17:26:18 +0800

    Party Games

--- aoapc-bac2nd/ch8/UVa1610.cc
+++ aoapc-bac2nd/ch8/UVa1610.cc
@@ -12,7 +12,7 @@ int main() {
     for(int i = 0; i < n; ++i) { cin >> S; D.push_back(S); }
     sort(D.begin(), D.end());
     S.clear();
-    int l = n/2-1, r = l+1, sz = D[l].size();
+    int l = n / 2 - 1, r = l + 1, sz = D[l].size();
     for(int i = 0; i < sz; ++i) {
       S += i < sz - 1 ? D[l][i] + 1 : D[l][i];
       if(S[i] != '[' && S < D[r]) break;
