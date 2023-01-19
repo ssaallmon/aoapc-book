f990ae840fd45e190ac9be0871dedb0b008882bd
yzs <921546507@qq.com>
2023-01-19 15:28:34 +0800

    Periodic Strings

--- aoapc-bac2nd/ch3/UVa455.cc
+++ aoapc-bac2nd/ch3/UVa455.cc
@@ -3,16 +3,16 @@
 
 int main() {
   int N;
+  char s[85];
   scanf("%d", &N);
   while(N--) {
-    char s[85];
     scanf("%s", s);
     int n = strlen(s);
     for(int k = 1; k <= n; ++k) {
       if(n % k) continue;
       int ok = 1;
-      for(int i = 1; ok && i < n / k; ++i)
-        if(strncmp(s, s + k * i, k)) ok = 0;
+      for(int i = k; ok && i < n; ++i)
+        if(s[i] != s[i % k]) ok = 0;
       if(ok) { printf("%d\n", k); break; }
     }
     if(N) putchar('\n');
