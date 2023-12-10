9bccd384422efbe22dce765fda9d0c6d510495a4
yzs <921546507@qq.com>
2023-12-10 09:22:32 +0800

    All in All

--- aoapc-bac2nd/ch3/UVa10340.cc
+++ aoapc-bac2nd/ch3/UVa10340.cc
@@ -5,10 +5,10 @@ const int maxn = 1e5 + 5;
 char s[maxn], t[maxn];
 
 int main() {
-  while(~scanf("%s%s", s, t)) {
+  while (~scanf("%s%s", s, t)) {
     int i = 0, n = strlen(t);
-    for(int j = 0; j < n; ++j)
-      if(t[j] == s[i]) ++i;
+    for (int j = 0; j < n; ++j)
+      if (t[j] == s[i]) ++i;
     puts(i == strlen(s) ? "Yes" : "No");
   }
   return 0;
