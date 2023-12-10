f7cfba88b2c7fa9dd12eccdc41d0cadd7bf671fd
yzs <921546507@qq.com>
2023-12-10 09:19:11 +0800

    WERTYU

--- aoapc-bac2nd/ch3/UVa10082.cc
+++ aoapc-bac2nd/ch3/UVa10082.cc
@@ -5,7 +5,7 @@ const char *s = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
 
 int main() {
   char c;
-  while(~(c = getchar()))
-    putchar(c == ' ' || c == '\n' ? c : s[strchr(s, c) - s - 1]);
+  while (~(c = getchar()))
+    putchar(c == ' ' || c == '\n' ? c : s[strchr(s, c)-s-1]);
   return 0;
 }
