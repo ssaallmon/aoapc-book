994bdcd2866ca359123b5693e3a23f7e92796258
yzs <921546507@qq.com>
2023-12-16 21:32:27 +0800

    Maximum Product

--- aoapc-bac2nd/ch7/UVa11059.cc
+++ aoapc-bac2nd/ch7/UVa11059.cc
@@ -3,14 +3,14 @@
 
 int main() {
   int N, M;
-  while(~scanf("%d", &N)) {
+  while (~scanf("%d", &N)) {
     int S[20];
     long long P = 0;
-    for(int i = 0; i < N; ++i) scanf("%d", &S[i]);
-    for(int i = 0; i < N; ++i) {
-      for(int j = i; j < N; ++j) {
+    for (int i = 0; i < N; ++i) scanf("%d", &S[i]);
+    for (int i = 0; i < N; ++i) {
+      for (int j = i; j < N; ++j) {
         long long a = 1;
-        for(int k = i; k <= j; ++k) a *= S[k];
+        for (int k = i; k <= j; ++k) a *= S[k];
         P = std::max(P, a);
       }
     }
