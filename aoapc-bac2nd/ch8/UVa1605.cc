7d5862a4f8929fc6b1ba77c16d128566de261660
yzs <921546507@qq.com>
2023-12-17 09:55:24 +0800

    Building for UN

--- aoapc-bac2nd/ch8/UVa1605.cc
+++ aoapc-bac2nd/ch8/UVa1605.cc
@@ -4,17 +4,16 @@ const char *s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int n;
 
 void build(int dir) {
-  for(int i = 0; i < n; ++i) {
-    for(int j = 0; j < n; ++j) putchar(s[dir ? i : j]);
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) putchar(s[dir ? i : j]);
     puts("");
   }
 }
 
 int main() {
   int kase = 0;
-  while(~scanf("%d", &n)) {
-    if(kase) putchar('\n');
-    ++kase;
+  while (~scanf("%d", &n)) {
+    if (kase++) putchar('\n');
     printf("2 %d %d\n", n, n);
     build(0); puts(""); build(1);
   }
