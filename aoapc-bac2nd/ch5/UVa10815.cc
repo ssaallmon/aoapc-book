677767e59d815aeef0d283f28e12e010eedf8274
yzs <921546507@qq.com>
2023-12-17 10:00:10 +0800

    Andy's First Dictionary

--- aoapc-bac2nd/ch5/UVa10815.cc
+++ aoapc-bac2nd/ch5/UVa10815.cc
@@ -8,15 +8,15 @@ int main() {
   string s;
   stringstream ss;
   set<string> dict;
-  while(getline(cin, s)) {
-    for(auto &c : s) {
-      if(isalpha(c)) c = tolower(c);
+  while (getline(cin, s)) {
+    for (auto& c : s) {
+      if (isalpha(c)) c = tolower(c);
       else c = ' ';
     }
     ss << s;
-    while(ss >> s) dict.insert(s);
+    while (ss >> s) dict.insert(s);
     ss.clear();
   }
-  for(auto &word : dict) cout << word << '\n';
+  for (auto& word : dict) cout << word << '\n';
   return 0;
 }
