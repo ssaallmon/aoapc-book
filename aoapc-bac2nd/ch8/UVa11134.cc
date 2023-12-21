bea6a21ac2d74356910058466f993896cc5aa844
yzs <921546507@qq.com>
2023-12-21 21:35:17 +0800

    Fabled Rooks

--- aoapc-bac2nd/ch8/UVa11134.cc
+++ aoapc-bac2nd/ch8/UVa11134.cc
@@ -13,8 +13,8 @@ struct Interval {
 } I[2][maxn];
 
 void place(int i, int j) {  
-  for(int k = I[i][j].l; k <= I[i][j].r; ++k) {
-    if(!vis[i][k]) {
+  for (int k = I[i][j].l; k <= I[i][j].r; ++k) {
+    if (!vis[i][k]) {
       p[i][I[i][j].id] = k;
       vis[i][k] = 1;
       ok &= 1;
@@ -26,18 +26,18 @@ void place(int i, int j) {
 
 int main() {
   int n;
-  while(scanf("%d", &n) && n) {
+  while (scanf("%d", &n) && n) {
     ok = 1;
     memset(vis, 0, sizeof(vis));
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
       scanf("%d%d%d%d", &I[0][i].l, &I[1][i].l, &I[0][i].r, &I[1][i].r);
       I[0][i].id = I[1][i].id = i;
     }
     std::sort(I[0], I[0] + n);
     std::sort(I[1], I[1] + n);
-    for(int i = 0; i < n && ok; ++i) { place(0, i); place(1, i); }
-    if(ok)
-      for(int i = 0; i < n; ++i) printf("%d %d\n", p[0][i], p[1][i]);
+    for (int i = 0; i < n && ok; ++i) { place(0, i); place(1, i); }
+    if (ok)
+      for (int i = 0; i < n; ++i) printf("%d %d\n", p[0][i], p[1][i]);
     else puts("IMPOSSIBLE");
   }
   return 0;
