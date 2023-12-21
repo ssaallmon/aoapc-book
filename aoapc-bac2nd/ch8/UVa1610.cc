3e6bfc9980e8dea18d0190bf18746f540e86c6dc
yzs <921546507@qq.com>
2023-12-21 21:32:33 +0800

    Party Games

--- aoapc-bac2nd/ch8/UVa1610.cc
+++ aoapc-bac2nd/ch8/UVa1610.cc
@@ -6,16 +6,16 @@ using namespace std;
 
 int main() {
   int n;
-  while(cin >> n && n) {
+  while (cin >> n && n) {
     string S;
     vector<string> D;
-    for(int i = 0; i < n; ++i) { cin >> S; D.push_back(S); }
+    for (int i = 0; i < n; ++i) { cin >> S; D.push_back(S); }
     sort(D.begin(), D.end());
     S.clear();
     int l = n / 2 - 1, r = l + 1, sz = D[l].size();
-    for(int i = 0; i < sz; ++i) {
+    for (int i = 0; i < sz; ++i) {
       S += i < sz - 1 ? D[l][i] + 1 : D[l][i];
-      if(S[i] != '[' && S < D[r]) break;
+      if (S[i] != '[' && S < D[r]) break;
       --S[i];
     }
     cout << S << '\n';
