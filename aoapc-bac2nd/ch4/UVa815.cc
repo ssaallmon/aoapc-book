d805dcfbd14835c213e1457d56ddb57c0716e3a3
yzs <921546507@qq.com>
2023-12-22 22:08:35 +0800

    Flooded!

--- aoapc-bac2nd/ch4/UVa815.cc
+++ aoapc-bac2nd/ch4/UVa815.cc
@@ -6,17 +6,17 @@ double v, k, wl; // v: volume, wl: water level
 
 int main() {
   int kase = 0;
-  while(scanf("%d%d", &m, &n) && m) {
+  while (scanf("%d%d", &m, &n) && m) {
     n *= m;
-    for(int i = 0; i < n; ++i) scanf("%d", &H[i]);
+    for (int i = 0; i < n; ++i) scanf("%d", &H[i]);
     scanf("%lf", &v);
     v /= 100; // volume -> elevation
     std::sort(H, H + n);
     H[n] = 0x7fffffff;
-    for(int i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i) {
       v += H[i - 1];
       wl = v / i;
-      if(wl < H[i]) { k = i; break; }
+      if (wl < H[i]) { k = i; break; }
     }
     printf("Region %d\nWater level is %.2f meters.\n", ++kase, wl);
     printf("%.2f percent of the region is under water.\n\n", k * 100 / n); 
