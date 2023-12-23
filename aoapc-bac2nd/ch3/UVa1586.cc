166813ee9a53a977355033efb36d5a46b4ed5dc4
yzs <921546507@qq.com>
2023-12-23 15:23:23 +0800

    Molar mass

--- aoapc-bac2nd/ch3/UVa1586.cc
+++ aoapc-bac2nd/ch3/UVa1586.cc
@@ -9,14 +9,14 @@ int main() {
   int T;
   char s[85];
   scanf("%d", &T);
-  while(T--) {
+  while (T--) {
     scanf("%s", s);
     double ans = .0;
     int n = strlen(s);
-    for(int i = 0; i < n; ++i) {
-      if(isdigit(s[i])) continue;
-      if(!isdigit(s[i + 1])) ans += wt[s[i]];
-      else ans += wt[s[i]] * (isdigit(s[i + 2]) ? (s[i + 1] - '0') * 10 + s[i + 2] - '0' : s[i + 1] - '0');
+    for (int i = 0; i < n; ++i) {
+      if (isdigit(s[i])) continue;
+      if (!isdigit(s[i + 1])) ans += wt[s[i]];
+      else ans += wt[s[i]] * (isdigit(s[i+2]) ? (s[i+1]-'0')*10 + s[i+2]-'0' : s[i+1]-'0');
     }
     printf("%.3f\n", ans);
   }
