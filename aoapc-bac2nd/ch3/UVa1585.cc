653f1a2ba559d05a2e54c2b3044d29ecfc200b25
yzs <921546507@qq.com>
2023-12-23 15:16:17 +0800

    Score

--- aoapc-bac2nd/ch3/UVa1585.cc
+++ aoapc-bac2nd/ch3/UVa1585.cc
@@ -4,9 +4,9 @@ int main() {
   int T;
   scanf("%d", &T);
   char c = getchar();
-  while(T--) {
+  while (T--) {
     int a = 0, b = 0;
-    while((c = getchar()) && c ^ '\n') c == 'O' ? b += ++a : a = 0;
+    while ((c = getchar()) && c ^ '\n') c == 'O' ? b += ++a : a = 0;
     printf("%d\n", b);
   }
   return 0;
