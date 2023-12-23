51dbf75f06cd3d8c0aa89f3c18ee97a6db36452d
yzs <921546507@qq.com>
2023-12-23 15:27:17 +0800

    Repeating Decimals

--- aoapc-bac2nd/ch3/UVa202.cc
+++ aoapc-bac2nd/ch3/UVa202.cc
@@ -5,21 +5,21 @@ int q[maxn], r[maxn];
 
 int main() {
   int a, b;
-  while(~scanf("%d%d", &a, &b)) {
+  while (~scanf("%d%d", &a, &b)) {
     int n = 0, ok = 1;
     printf("%d/%d = %d.", a, b, a / b);
     a %= b;
-    while(ok) {
+    while (ok) {
       a *= 10;
       int c = a / b;  // quotient
       a %= b;         // remainder
-      for(int i = 0; i < n; ++i) {
-        if(q[i] == c && r[i] == a) {
-          for(int j = 0; j < i; ++j) printf("%d", q[j]);
+      for (int i = 0; i < n; ++i) {
+        if (q[i] == c && r[i] == a) {
+          for (int j = 0; j < i; ++j) printf("%d", q[j]);
           putchar('(');
           int len = n < 50 ? n : 50;
-          for(int j = i; j < len; ++j) printf("%d", q[j]);
-          if(n >= 50) printf("...");
+          for (int j = i; j < len; ++j) printf("%d", q[j]);
+          if (n >= 50) printf("...");
           printf(")\n   %d = number of digits in repeating cycle\n\n", n - i);
           ok = 0;
           break;
