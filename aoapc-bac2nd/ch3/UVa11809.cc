e428ebbed9bb4b4aab1578d0ceb1b24408516b93
yzs <921546507@qq.com>
2023-12-23 15:12:01 +0800

    Floating-Point Numbers

--- aoapc-bac2nd/ch3/UVa11809.cc
+++ aoapc-bac2nd/ch3/UVa11809.cc
@@ -8,9 +8,9 @@ long long b[10][35];
 const float EPS = 1e-4;
 
 void init() {
-  for(int M = 0; M < 10; ++M) {
-    for(int E = 1; E < 31; ++E) {
-      double d = log10(1 - pow(2, -1 - M)) + (pow(2, E) - 1) * log10(2);
+  for (int M = 0; M < 10; ++M) {
+    for (int E = 1; E < 31; ++E) {
+      double d = log10(1 - pow(2, -1-M)) + (pow(2, E) - 1) * log10(2);
       b[M][E] = d;
       a[M][E] = pow(10, d - b[M][E]);
     }
@@ -18,9 +18,9 @@ void init() {
 }
 
 void print_ans() {
-  for(int M = 0; M < 10; ++M) {
-    for(int E = 1; E < 31; ++E) {
-      if(b[M][E] == B && fabs(A - a[M][E]) < EPS) {
+  for (int M = 0; M < 10; ++M) {
+    for (int E = 1; E < 31; ++E) {
+      if (b[M][E] == B && fabs(A - a[M][E]) < EPS) {
         printf("%d %d\n", M, E);
         return;
       }
@@ -31,7 +31,7 @@ void print_ans() {
 int main() {
   init();
   char F[30];
-  while(scanf("%s", F) && strcmp(F, "0e0")) {
+  while (scanf("%s", F) && strcmp(F, "0e0")) {
     *strchr(F, 'e') = ' ';
     sscanf(F, "%lf%d", &A, &B);  // A * 10^B = C * 2^D;
     print_ans();
