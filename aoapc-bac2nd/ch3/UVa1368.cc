500ac65f428c284782949b3d42c09327b2e34683
yzs <921546507@qq.com>
2023-12-23 15:14:01 +0800

    DNA Consensus String

--- aoapc-bac2nd/ch3/UVa1368.cc
+++ aoapc-bac2nd/ch3/UVa1368.cc
@@ -7,17 +7,17 @@ char DNA[55][1005];
 int main() {
   int T;
   scanf("%d", &T);
-  while(T--) {
+  while (T--) {
     int err = 0;
     char ans[1005]{};
     scanf("%d%d", &m, &n);
     memset(DNA, 0, sizeof(DNA));
-    for(int i = 0; i < m; ++i) scanf("%s", DNA[i]);
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < m; ++i) scanf("%s", DNA[i]);
+    for (int i = 0; i < n; ++i) {
       int num[26]{}, k = 0;
-      for(int j = 0; j < m; ++j) ++num[DNA[j][i] - 'A'];
-      for(int j = 0; j < 26; ++j)
-        if(num[j] > num[k]) k = j;
+      for (int j = 0; j < m; ++j) ++num[DNA[j][i] - 'A'];
+      for (int j = 0; j < 26; ++j)
+        if (num[j] > num[k]) k = j;
       ans[i] = 'A' + k;
       err += m - num[k];
     }
