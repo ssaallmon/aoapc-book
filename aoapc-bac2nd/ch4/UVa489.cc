d392f74e925198aab3e26b0a4688322c60ad376e
yzs <921546507@qq.com>
2023-12-24 15:27:55 +0800

    Hangman Judge

--- aoapc-bac2nd/ch4/UVa489.cc
+++ aoapc-bac2nd/ch4/UVa489.cc
@@ -9,30 +9,30 @@ int guessed[256];
 
 void guess(char c) {
   int bad = 1;
-  for(int i = 0; i < strlen(s); ++i)
-    if(s[i] == c) { s[i] = ' '; --left; bad = 0; }
-  if(bad) --chance;
-  if(!left) win = 1;
-  if(!chance) lose = 1;
+  for (int i = 0; i < strlen(s); ++i)
+    if (s[i] == c) { s[i] = ' '; --left; bad = 0; }
+  if (bad) --chance;
+  if (!left) win = 1;
+  if (!chance) lose = 1;
 }
 
 int main() {
   int rnd;
-  while(scanf("%d%s%s", &rnd, s, s2) && rnd != -1) {
+  while (scanf("%d%s%s", &rnd, s, s2) && rnd != -1) {
     printf("Round %d\n", rnd);
     win = lose = 0;
     left = strlen(s);
     chance = 7;
     memset(guessed, 0, sizeof(guessed));
-    for(int i = 0; i < strlen(s2); ++i) {
-      if(!guessed[s2[i]]) {
+    for (int i = 0; i < strlen(s2); ++i) {
+      if (!guessed[s2[i]]) {
         guess(s2[i]);
         guessed[s2[i]] = 1;
-        if(win || lose) break;
+        if (win || lose) break;
       }
     }
-    if(win) printf("You win.\n");
-    else if(lose) printf("You lose.\n");
+    if (win) printf("You win.\n");
+    else if (lose) printf("You lose.\n");
     else printf("You chickened out.\n");
   }
   return 0;
