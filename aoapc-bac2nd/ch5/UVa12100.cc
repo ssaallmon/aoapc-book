ccdd6e185439ecf1d4e061cfe825c6e1807d8b09
yzs <921546507@qq.com>
2023-12-25 21:31:07 +0800

    Printer Queue

--- aoapc-bac2nd/ch5/UVa12100.cc
+++ aoapc-bac2nd/ch5/UVa12100.cc
@@ -10,19 +10,19 @@ int n, m;
 int main() {
   int T;
   scanf("%d", &T);
-  while(T--) {
+  while (T--) {
     int num = 0;
     queue<Job> q;
     priority_queue<int> pq;
     scanf("%d%d", &n, &m);
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
       int p;
       scanf("%d", &p);
       q.push(Job{i, p});
       pq.push(p);
     }
-    while(q.front().id != m || q.front().priority != pq.top()) {
-      if(q.front().priority == pq.top()) {
+    while (q.front().id != m || q.front().priority != pq.top()) {
+      if (q.front().priority == pq.top()) {
         ++num;
         q.pop();
         pq.pop();
