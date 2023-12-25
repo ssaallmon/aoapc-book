47883fd8ab4f09d70867aa07cffc690a41fee7bb
yzs <921546507@qq.com>
2023-12-25 21:33:30 +0800

    Updating a Dictionary

--- aoapc-bac2nd/ch5/UVa12504.cc
+++ aoapc-bac2nd/ch5/UVa12504.cc
@@ -10,41 +10,41 @@ map<string, string> dict[2];
 set<string> ans[3];
 
 void find_out() {
-  for(auto &p : dict[0]) {
+  for (auto& p : dict[0]) {
     string key = p.first;
-    if(dict[1].count(key)) {
-      if(dict[0][key] != dict[1][key]) ans[2].insert(key);
+    if (dict[1].count(key)) {
+      if (dict[0][key] != dict[1][key]) ans[2].insert(key);
       dict[1].erase(key);
     } else ans[1].insert(key);
   }
-  for(auto &p : dict[1]) ans[0].insert(p.first);
+  for (auto& p : dict[1]) ans[0].insert(p.first);
 }
 
 void print_ans() {
   int changed = 0;
-  for(int i = 0; i < 3; ++i) {
-    if(ans[i].empty()) continue;
+  for (int i = 0; i < 3; ++i) {
+    if (ans[i].empty()) continue;
     changed = 1;
     cout << prefix[i];
-    for(auto &k : ans[i]) cout << k << (k == *ans[i].rbegin() ? '\n' : ',');
+    for (auto& k : ans[i]) cout << k << (k == *ans[i].rbegin() ? '\n' : ',');
   }
-  if(!changed) cout << "No changes\n";
+  if (!changed) cout << "No changes\n";
 }
 
 int main() {
   int T;
   cin >> T;
-  while(T--) {
+  while (T--) {
     dict[0].clear();
     dict[1].clear();
-    for(int i = 0; i < 3; ++i) ans[i].clear();
-    for(int i = 0; i < 2; ++i) {
+    for (int i = 0; i < 3; ++i) ans[i].clear();
+    for (int i = 0; i < 2; ++i) {
       string s, key, val;
       cin >> s;
-      for(auto &c : s)
-        if(!isalnum(c)) c = ' ';
+      for (auto& c : s)
+        if (!isalnum(c)) c = ' ';
       stringstream ss(s);
-      while(ss >> key >> val) dict[i][key] = val;
+      while (ss >> key >> val) dict[i][key] = val;
     }
     find_out();
     print_ans();
