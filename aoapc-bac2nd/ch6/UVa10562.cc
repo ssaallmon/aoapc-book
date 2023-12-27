7b067bbd5481cbb19d3b84cd9e62407015fda219
yzs <921546507@qq.com>
2023-12-27 21:44:35 +0800

    Undraw the Trees

--- aoapc-bac2nd/ch6/UVa10562.cc
+++ aoapc-bac2nd/ch6/UVa10562.cc
@@ -8,11 +8,11 @@ int printable(char c) { return c != '-' && c != '|' && c != ' ' && c != '#' && c
 
 void undraw(int r, int c) {
   printf("%c(", tree[r][c]);
-  if(tree[r + 1][c] == '|') {
+  if (tree[r + 1][c] == '|') {
     int k = c, n = strlen(tree[r + 3]);
-    while(k && tree[r + 2][k - 1] == '-') --k;
-    while(k < n && tree[r + 2][k] != ' ') {
-      if(printable(tree[r + 3][k])) undraw(r + 3, k);
+    while (k && tree[r + 2][k - 1] == '-') --k;
+    while (k < n && tree[r + 2][k] != ' ') {
+      if (printable(tree[r + 3][k])) undraw(r + 3, k);
       ++k;
     } 
   }
@@ -23,14 +23,14 @@ int main() {
   int T;
   scanf("%d", &T);
   getchar();
-  while(T--) {
+  while (T--) {
     int i = 0;
     memset(tree, 0, sizeof(tree));
-    while(*fgets(tree[i++], maxn, stdin) != '#');
+    while (*fgets(tree[i++], maxn, stdin) != '#');
     int n = strlen(tree[0]);
     putchar('(');
-    for(int i = 0; i < n; ++i)
-      if(printable(tree[0][i])) { undraw(0, i); break; }
+    for (int i = 0; i < n; ++i)
+      if (printable(tree[0][i])) { undraw(0, i); break; }
     puts(")");
   }
   return 0;
