42b26b41ed8a7d4e24b24dbdec00c973c934b499
yzs <921546507@qq.com>
2023-12-30 21:27:20 +0800

    Accordian Patience

--- aoapc-bac2nd/ch6/UVa127.cc
+++ aoapc-bac2nd/ch6/UVa127.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 struct Card {
   char rank, suit;
-  bool operator==(const Card &rhs) const {
+  bool operator==(const Card& rhs) const {
     return rank == rhs.rank || suit == rhs.suit;
   }
 };
@@ -20,35 +20,35 @@ void link(int L, int R) {
 }
 
 int find(int idx, int n) {
-  while(idx && n--) idx = left[idx];
+  while (idx && n--) idx = left[idx];
   return idx;
 }
 
 void accordian(int idx) {
-  if(!idx) return;
+  if (!idx) return;
   int left_pos = 0;
-  for(int i = idx; i; i = right[i]) {
-    for(int j = 3; j > 0; j -= 2) {
+  for (int i = idx; i; i = right[i]) {
+    for (int j = 3; j > 0; j -= 2) {
       int k = find(i, j);
-      if(k && pile[k].top() == pile[i].top()) {
+      if (k && pile[k].top() == pile[i].top()) {
         pile[k].push(pile[i].top());
         pile[i].pop();
         left_pos = k;
-        if(pile[i].empty()) { --cnt; link(left[i], right[i]); }
+        if (pile[i].empty()) { --cnt; link(left[i], right[i]); }
         break;
       }
     }
-    if(left_pos) break;
+    if (left_pos) break;
   }
   accordian(left_pos);
 }
 
 int main() {
   char s[3];
-  while(scanf("%s", s) && s[0] != '#') {
+  while (scanf("%s", s) && s[0] != '#') {
     pile = vector<stack<Card>>(53, stack<Card>());
     pile[1].push(Card{s[0], s[1]});
-    for(int i = 2; i < 53; ++i) {
+    for (int i = 2; i < 53; ++i) {
       scanf("%s", s);
       pile[i].push(Card{s[0], s[1]});
       link(i - 1, i);
@@ -58,7 +58,7 @@ int main() {
     link(52, 0);
     accordian(right[0]);
     printf("%d pile%sremaining:", cnt, cnt > 1 ? "s " : " ");
-    for(int i = right[0]; i; i = right[i]) printf(" %d", pile[i].size());
+    for (int i = right[0]; i; i = right[i]) printf(" %d", pile[i].size());
     putchar('\n');
   }
   return 0;
