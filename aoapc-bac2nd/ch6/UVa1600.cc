87996795a257e92a4ba7a627fb225960cd11d7c5
yzs <921546507@qq.com>
2023-12-31 09:14:20 +0800

    Patrol Robot

--- aoapc-bac2nd/ch6/UVa1600.cc
+++ aoapc-bac2nd/ch6/UVa1600.cc
@@ -23,13 +23,13 @@ int bfs() {
   std::queue<Cell> q;
   q.push(o);
   d[0][0][k] = 0;
-  while(!q.empty()) {
+  while (!q.empty()) {
     Cell u = q.front();
     q.pop();
-    if(u.r == m - 1 && u.c == n - 1) return d[u.r][u.c][u.b];
-    for(int i = 0; i < 4; ++i) {
+    if (u.r == m - 1 && u.c == n - 1) return d[u.r][u.c][u.b];
+    for (int i = 0; i < 4; ++i) {
       Cell v = walk(u, i);
-      if(inside(v.r, v.c) && v.b >= 0 && d[v.r][v.c][v.b] < 0) {
+      if (inside(v.r, v.c) && v.b >= 0 && d[v.r][v.c][v.b] < 0) {
         d[v.r][v.c][v.b] = d[u.r][u.c][u.b] + 1;
         q.push(v);
       }
@@ -41,10 +41,10 @@ int bfs() {
 int main() {
   int T;
   scanf("%d", &T);
-  while(T--) {
+  while (T--) {
     scanf("%d%d%d", &m, &n, &k);
-    for(int i = 0; i < m; ++i)
-      for(int j = 0; j < n; ++j) scanf("%d", &G[i][j]);
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j) scanf("%d", &G[i][j]);
     memset(d, -1, sizeof(d));
     printf("%d\n", bfs());
   }
