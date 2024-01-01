784b12e0ba729a083089ab7ccf1a927a6ac86ccc
yzs <921546507@qq.com>
2024-01-01 11:00:09 +0800

    Argus

--- TrainingGuide/ch3/LA3135.cc
+++ TrainingGuide/ch3/LA3135.cc
@@ -3,7 +3,7 @@
 
 struct Register {
   int Q_num, Period, Time;  // Time: Next execution time
-  bool operator<(const Register &rhs) const {
+  bool operator<(const Register& rhs) const {
     return Time > rhs.Time || Time == rhs.Time && Q_num > rhs.Q_num;
   }
 };
@@ -13,13 +13,13 @@ int main() {
   char s[10];
   Register r;
   std::priority_queue<Register> pq;
-  while(scanf("%s", s) && s[0] != '#') {
+  while (scanf("%s", s) && s[0] != '#') {
     scanf("%d%d", &r.Q_num, &r.Period);
     r.Time = r.Period;
     pq.push(r);
   }
   scanf("%d", &K);
-  while(K--) {
+  while (K--) {
     r = pq.top();
     pq.pop();
     printf("%d\n", r.Q_num);
