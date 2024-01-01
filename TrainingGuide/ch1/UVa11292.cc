164009cf7776d93b59146fbb0afe64b94117c2ae
yzs <921546507@qq.com>
2024-01-01 10:44:07 +0800

    Dragon of Loowater

--- TrainingGuide/ch1/UVa11292.cc
+++ TrainingGuide/ch1/UVa11292.cc
@@ -7,19 +7,19 @@ int A[maxn], B[maxn];
 
 int main() {
   int n, m;
-  while(scanf("%d%d", &n, &m) && n) {
+  while (scanf("%d%d", &n, &m) && n) {
     int wage = 0, cur = 0;
-    for(int i = 0; i < n; ++i) scanf("%d", &A[i]);
-    for(int i = 0; i < m; ++i) scanf("%d", &B[i]);
+    for (int i = 0; i < n; ++i) scanf("%d", &A[i]);
+    for (int i = 0; i < m; ++i) scanf("%d", &B[i]);
     sort(A, A + n);
     sort(B, B + m);
-    for(int i = 0; i < m; ++i) {
-      if(A[cur] <= B[i]) {
+    for (int i = 0; i < m; ++i) {
+      if (A[cur] <= B[i]) {
         wage += B[i];
-        if(++cur == n) break;
+        if (++cur == n) break;
       }
     }
-    if(cur < n) puts("Loowater is doomed!");
+    if (cur < n) puts("Loowater is doomed!");
     else printf("%d\n", wage);
   }
   return 0;
