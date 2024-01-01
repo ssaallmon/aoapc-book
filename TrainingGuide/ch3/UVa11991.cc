67a288f1263fed205ea76268f38bc907e053273d
yzs <921546507@qq.com>
2024-01-01 11:03:51 +0800

    Easy Problem from Rujia Liu?

--- TrainingGuide/ch3/UVa11991.cc
+++ TrainingGuide/ch3/UVa11991.cc
@@ -7,13 +7,13 @@ int n, m, k, v;
 map<int, vector<int>> a;
 
 int main() {
-  while(~scanf("%d%d", &n, &m)) {
-    for(int i = 1; i <= n; ++i) {
+  while (~scanf("%d%d", &n, &m)) {
+    for (int i = 1; i <= n; ++i) {
       scanf("%d", &v);
-      if(!a.count(v)) a[v] = vector<int>();
+      if (!a.count(v)) a[v] = vector<int>();
       a[v].push_back(i);
     }
-    while(m--) {
+    while (m--) {
       scanf("%d%d", &k, &v);
       printf("%d\n", !a.count(v) || k > a[v].size() ? 0 : a[v][k - 1]);
     }
